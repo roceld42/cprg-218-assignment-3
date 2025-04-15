16caf1589090437d966d3a451ce87d771b051f2f
Rocel <deleonrocel42@gmail.com>
2025-04-15 00:24:34 -0600

    docs: remove api link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // weather api
 function getWeather() {
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat={21.17429}&lon={-86.84656}&appid={34240611284300c2fe746df99d0e11d2}')
+    fetch()
     .then((response) => response.json())
     .then((data) => {
         console.log(data);
