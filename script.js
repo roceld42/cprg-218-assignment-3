98f974958cc87adcce1e4d6ddad913a86e070b91
Rocel <deleonrocel42@gmail.com>
2025-04-15 23:04:22 -0600

    docs: delete videos, add weather api, add media queries

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// weather api
+let apikey = 'ab12b65569969fd5fb077dd1037b5415';
+let city = 'Cancun';
+let lat = 21.17429
+let lon = -86.84656
+
+getWeather()
+
+function getWeather() {
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat=21.17429&lon=-86.84656&appid=ab12b65569969fd5fb077dd1037b5415&units=metric') 
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        document.getElementById('cityname').textContent = data.name;
+        document.getElementById('temp').textContent = `${data.main.temp}°C`;
+        document.getElementById('weatherdescription').textContent = data.weather[0].description;
+
+        let icon = data.weather[0].icon
+        let iconurl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+        document.getElementById('weathericon').src = iconurl
+
+    }).catch((error) => {
+        console.log('ERROR', error);
+    })
+}
 
 // footer
 const d = new Date();
